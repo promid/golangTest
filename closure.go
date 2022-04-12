46e66e4b65fa44a8eb3d90df351f21d5173f1282
bedong <bedong@ebay.com>
2022-04-12 16:51:29 +0800

    closure

--- closure.go
+++ closure.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := closure(1)(2)
+	fmt.Println(a)
+}
+
+func closure(x int) func(int) int {
+	return func(y int) int {
+		return x + y
+	}
+}
