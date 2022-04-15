2ce4aded9fbb69651173febd4fe7c8002f3ba55a
bedong <bedong@ebay.com>
2022-04-15 12:16:37 +0800

    泛型

---
+++ generics.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Number interface {
+	int64 | float64
+}
+
+// SumNumbers sums the values of map m. Its supports both integers
+// and floats as map values.
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func main() {
+	ints := map[string]int64{
+		"first":  34,
+		"second": 12,
+	}
+
+	// Initialize a map for the float values
+	floats := map[string]float64{
+		"first":  35.98,
+		"second": 26.99,
+	}
+
+	fmt.Printf("Non-Generic Sums: %v and %v\n",
+		SumNumbers(ints),
+		SumNumbers(floats))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module testProject
 
-go 1.17
+go 1.18
