80eb1678ae280b6c57379590e28d5924e3b9bdd4
bedong <bedong@ebay.com>
2022-04-21 20:53:39 +0800

    update

--- interface_cases/interface_nil_test.go
+++ interface_cases/interface_nil_test.go
@@ -8,11 +8,13 @@ import (
 func Test(t *testing.T) {
 	var i interface{}
 	// 只有当接口存储的类型和对象都为nil时，接口才等于nil
-	fmt.Println(i == nil) // false
+	fmt.Println(i == nil) // true
 	fmt.Println(i)        // <nil>
 
 	var p *int
 	i = p                 // 存储类型是一个指针
-	fmt.Println(i == nil) // false
+	fmt.Println(p == nil) // true
+	fmt.Println(i == nil) // attention: false
+	fmt.Println(i == p)   // true
 	fmt.Println(i)        // <nil>
 }
