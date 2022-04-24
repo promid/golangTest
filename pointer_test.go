f22b88b6e14d356c3473b02bb095d86179642fe7
bedong <bedong@ebay.com>
2022-04-24 15:42:30 +0800

    pointer

---
+++ pointer_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+)
+
+type Integer int
+
+func (a *Integer) Add(b Integer) {
+	*a += b
+}
+
+func TestAdd(t *testing.T) {
+	var a Integer = 1
+	a.Add(2)
+	if a != 3 {
+		t.Error("Add() failed, got", a, "expected 3")
+	}
+	t.Log("Add() passed")
+}
