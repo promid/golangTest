f18f98aaa2ab02354bf01c6f9b49310a32c37c65
bedong <bedong@ebay.com>
2022-04-25 11:40:29 +0800

    update

--- once_test.go
+++ once_test.go
@@ -1,28 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"sync"
 	"testing"
 )
 
-var a string
 var once sync.Once
 
-func setup() {
-	a = "hello, world"
+func doPrint(doneCh chan struct{}) {
+	// once的Do()方法可以保证在全局范围内只调用指定的函数一次,而且所有其他goroutine在调用到此语句时,将会先被阻塞,直至全局唯一的once.Do()调用结束后才继续
+	once.Do(func() {
+		fmt.Println("hello world")
+	})
+	fmt.Println("after once.Do")
+	doneCh <- struct{}{}
 }
 
-func doPrint() {
-	// once 的 Do()方法可以保证在全局范围内只调用指定的函数一次（这里指 setup() 函数）， 而且所有其他goroutine在调用到此语句时， 将会先被阻塞， 直至全局唯一的 once.Do()调用结束后才继续
-	once.Do(setup)
-	print(a)
-}
-
-func twoPrint() {
-	go doPrint()
-	go doPrint()
+func twoPrint(doneCh chan struct{}) {
+	go doPrint(doneCh)
+	go doPrint(doneCh)
 }
 
 func TestOnce(t *testing.T) {
-	twoPrint()
+	doneCh := make(chan struct{}, 2)
+	twoPrint(doneCh)
+	for i := 0; i < 2; i++ {
+		<-doneCh
+	}
 }
