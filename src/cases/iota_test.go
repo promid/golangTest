18ef5f0433f17abffe7d73b82bc221f8e686a3ff
bedong <bedong@ebay.com>
2022-05-18 14:09:49 +0800

    update itoa

--- src/cases/iota_test.go
+++ src/cases/iota_test.go
@@ -11,6 +11,17 @@ const (
 	DD
 )
 
+const (
+	mutexLocked      = 1 << iota // 1 左移 0
+	mutexWoken                   // 1 左移 1 => 10 = 2
+	mutexStarving                // 1 左移 2 => 100 = 4
+	mutexWaiterShift = iota      // 3
+
+	starvationThresholdNs = 1e6
+)
+
 func Test1(t *testing.T) {
 	t.Log(AA, BB, CC, DD)
+	t.Log(mutexLocked, mutexWoken, mutexStarving, mutexWaiterShift)
+	t.Log(starvationThresholdNs)
 }
