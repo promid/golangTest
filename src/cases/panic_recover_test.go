a8e4458195750a8455acb86943ed0b330efe6d7f
bedong <bedong@ebay.com>
2022-05-18 10:12:05 +0800

    panic recover update

--- src/cases/panic_recover_test.go
+++ src/cases/panic_recover_test.go
@@ -1,8 +1,10 @@
 package cases
 
 import (
+	"context"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func CatchPanic() {
@@ -19,6 +21,28 @@ func TestRecoverPanic(t *testing.T) {
 	//  处理异常的函数
 	defer CatchPanic()
 	exceptionFun()
+}
+
+func TestRecoverPanic2(t *testing.T) {
+	//  处理异常的函数
+	defer CatchPanic()
+	go exceptionFun() // panic won't be caught in another goroutine
+	ctx, cancel := context.WithCancel(context.TODO())
+	go func() {
+		num := 1
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println("done")
+			default:
+				fmt.Println(num)
+				num++
+				time.Sleep(1 * time.Second)
+			}
+		}
+	}()
+	time.Sleep(5 * time.Second)
+	cancel()
 }
 
 func exceptionFun() {
