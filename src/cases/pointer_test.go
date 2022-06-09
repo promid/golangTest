37d791eb837194d22f7855ab4321dc421c2828c0
bedong <bedong@ebay.com>
2022-06-09 18:40:04 +0800

    slice pointer memory leak

--- src/cases/pointer_test.go
+++ src/cases/pointer_test.go
@@ -2,6 +2,7 @@ package cases
 
 import (
 	"testing"
+	"time"
 )
 
 type Integer int
@@ -17,4 +18,35 @@ func TestAdd(t *testing.T) {
 		t.Error("Add() failed, got", a, "expected 3")
 	}
 	t.Log("Add() passed")
+}
+
+func TestPointerArray(t *testing.T) {
+	type Stu struct {
+		Name string
+		Age  int
+	}
+	var students []Stu
+	students = append(students, Stu{Name: "张三", Age: 18})
+	students = append(students, Stu{Name: "李四", Age: 19})
+
+	var studentsP []*Stu
+
+	//slice pointer memory leak
+	//wrong ⬇️
+	//for _, stu := range students {
+	//	studentsP = append(studentsP, &stu)
+	//}
+
+	for index := range students {
+		studentsP = append(studentsP, &students[index])
+	}
+
+	for _, stu := range students {
+		stu := stu
+		go func() {
+			t.Log(stu.Name, stu.Age)
+		}()
+	}
+
+	time.Sleep(time.Second)
 }
