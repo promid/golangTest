25b7df921733a4b3b573b2be83c8b51a2872b66d
bedong <bedong@ebay.com>
2023-02-20 09:40:24 +0800

    update

--- src/cases/split_test.go
+++ src/cases/split_test.go
@@ -12,4 +12,18 @@ func TestSplit(t *testing.T) {
 	t.Log(len(strings.Split(str, ":")))
 	str = "kind-"
 	t.Log(len(strings.Split(str, "kind-")))
+	str = ""
+	t.Log(len(strings.Split(str, ":")))
+}
+
+func TestSplitN(t *testing.T) {
+	str := "a=b=c"
+	t.Log(strings.SplitN(str, "=", 2))
+	t.Log(strings.SplitN(str, "=", 3))
+	str = "abc"
+	t.Log(strings.SplitN(str, "=", 2))
+	t.Log(len(strings.SplitN(str, "=", 2)))
+	str = "a="
+	t.Log(strings.SplitN(str, "=", 2))
+	t.Log(len(strings.SplitN(str, "=", 2)))
 }
