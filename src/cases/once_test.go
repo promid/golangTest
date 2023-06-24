b1fb4afc63bca4717416ba82fe7d1910f884cbd0
bedong <bedong@ebay.com>
2023-06-24 22:47:35 +0800

    update once

--- src/cases/once_test.go
+++ src/cases/once_test.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"sync"
 	"testing"
+	"time"
 )
 
 var once sync.Once
+var once2 sync.Once
 
 func doPrint(doneCh chan struct{}) {
 	// once的Do()方法可以保证在全局范围内只调用指定的函数一次,而且所有其他goroutine在调用到此语句时,将会先被阻塞,直至全局唯一的once.Do()调用结束后才继续
@@ -28,4 +30,18 @@ func TestOnce(t *testing.T) {
 	for i := 0; i < 2; i++ {
 		<-doneCh
 	}
+}
+
+func doOnce2() {
+	once2.Do(func() {
+		fmt.Println("1")
+		time.Sleep(2 * time.Second)
+		fmt.Println("2")
+	})
+	// will wait until once2.Do() is done
+	fmt.Println("3")
+}
+
+func TestOnce2(t *testing.T) {
+	doOnce2()
 }
