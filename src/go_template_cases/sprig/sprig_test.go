eaedc6792fedc48eb3ed2af2ddff7ee0b322d1a8
bedong <bedong@ebay.com>
2024-05-20 09:47:55 +0800

    add sprig test case

--- src/go_template_cases/sprig/sprig_test.go
+++ src/go_template_cases/sprig/sprig_test.go
@@ -12,11 +12,26 @@ type SprigTest struct {
 	Name string
 }
 
+type Cluster struct {
+	Name    string
+	Address string
+}
+
 func TestSprig(t *testing.T) {
 	s := &SprigTest{
 		Name: "bei",
 	}
 	templ, _ := template.New("").Funcs(sprig.TxtFuncMap()).Parse("{{ .Name | upper }}")
+	buffer := &bytes.Buffer{}
+	_ = templ.Execute(buffer, s)
+	println(buffer.String())
+}
+
+func TestSprig2(t *testing.T) {
+	s := &Cluster{
+		Name: "xxx999",
+	}
+	templ, _ := template.New("").Funcs(sprig.TxtFuncMap()).Parse(`{{ .Address | default ("https//api.system.svc.TODO.abc.io" | replace "TODO" (trimPrefix "xxx" .Name) ) }}`)
 	buffer := &bytes.Buffer{}
 	_ = templ.Execute(buffer, s)
 	println(buffer.String())
